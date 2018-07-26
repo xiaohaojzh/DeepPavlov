2d5a5607a52344f8151212682ab7e69b8b536f51
nikolay-bushkov <bushkov@phystech.edu>
2018-07-26 15:59:08 +0300

    docs: improve core.data and Config description

--- deeppavlov/core/data/data_fitting_iterator.py
+++ deeppavlov/core/data/data_fitting_iterator.py
@@ -1,18 +1,16 @@
-"""
-Copyright 2017 Neural Networks and Deep Learning lab, MIPT
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-"""
+# Copyright 2017 Neural Networks and Deep Learning lab, MIPT
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
 from random import Random
 from typing import List, Generator, Tuple, Any, Optional
@@ -27,7 +25,7 @@ logger = get_logger(__name__)
 class DataFittingIterator:
     """
     Dataset iterator for fitting models, e. g. vocabs, kNN, vectorizers.
-    Data is passed as a list of strings(documents).
+    Data is passed as a list of strings (documents).
     Generate batches (for large datasets).
     """
 
--- docs/apiref/core/data.rst
+++ docs/apiref/core/data.rst
@@ -1,8 +1,21 @@
 core.data
 =========
-Basic DatasetIterator, DatasetReader and Vocab classes.
+DatasetReader, Vocab, DataLearningIterator and DataFittingIterator classes.
 
 .. automodule:: deeppavlov.core.data
-   :members:
+
+.. automodule:: deeppavlov.core.data.dataset_reader
+
+.. autoclass:: deeppavlov.core.data.dataset_reader.DatasetReader
+
+.. automodule:: deeppavlov.core.data.data_learning_iterator
+
+.. autoclass:: deeppavlov.core.data.data_learning_iterator.DataLearningIterator
+
+.. automodule:: deeppavlov.core.data.data_fitting_iterator
+
+.. autoclass:: deeppavlov.core.data.data_fitting_iterator.DataFittingIterator
+
+.. automodule:: deeppavlov.core.data.sqlite_database
 
 .. autoclass:: deeppavlov.core.data.sqlite_database.Sqlite3Database
